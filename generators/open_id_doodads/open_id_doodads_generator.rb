6e956493940c74e5ee6ffc1ea58e2d9f09efe8fa
Philip (flip) Kromer <flip@infochimps.org>
2008-06-09 05:23:20 -0500

    initial commit

---
+++ generators/open_id_doodads/open_id_doodads_generator.rb
@@ -0,0 +1,8 @@
+class Vendor::Plugins::OpenIdDoodadsGenerator < Rails::Generator::NamedBase
+  def manifest
+    record do |m|
+      # m.directory "lib"
+      # m.template 'README', "README"
+    end
+  end
+end
---
+++ tasks/open_id_doodads_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :open_id_doodads do
+#   # Task goes here
+# end
